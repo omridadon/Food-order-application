114fa2d6cc0ec87d6dcaea0befd9765947e81525
Omri Dadon <omridadon@gmail.com>
2021-12-18 14:18:02 +0200

    final order app with all the components

---
+++ src/component/Meals/Meals.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+import MealsSummary from "./MealsSummary";
+import AvilableMeals from "./AvilableMeals";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvilableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
